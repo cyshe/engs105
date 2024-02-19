5a9a2e8a1f4d6d83915cb8318c8645d2c7731236
unknown <cyshe3@gmail.com>
2024-02-19 01:27:59 -0500

    hw4 done add hw5 files

--- hw4/hw4.f90
+++ hw4/hw4.f90
@@ -1,22 +1,12 @@
     program hw4
     implicit none
-    
-    interface
-        subroutine element_matrix(l, ele, node, a_ele, b_ele)
-            implicit none
-            integer, intent(in) :: l 
-            integer, allocatable, intent(in) :: ele(:, :)
-            real, allocatable, intent(in) :: node(:, :)
-            real, allocatable, intent(out) :: a_ele(:, :), b_ele(:)
-        end subroutine element_matrix
-    end interface
-    
 
-    integer :: io, n_ele = 38, n_node=28, l, i, j, ii, jj, jb, hbw=10
+    integer :: io=6, n_ele = 38, n_node=28, l, i, j, ii, jj, jb, hbw=6
     integer, allocatable :: ele(:, :)
-    real, allocatable :: node(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:)
+    real, allocatable :: node(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:), dx(:), dy(:)
+    real :: x1, x2, x3, y1, y2, y3, area, dx1, dx2, dx3, dy1, dy2, dy3, ke = 0
 
-    allocate(ele(n_ele, 5), node(n_node, 3), a(n_node, n_node), b(n_node), a_ele(3, 3), b_ele(3))
+    allocate(ele(n_ele, 5), node(n_node, 3), a(n_node, n_node), b(n_node), a_ele(3, 3), b_ele(3), dx(n_ele), dy(n_ele))
     
     ele = 0
     node = 0
@@ -24,81 +14,25 @@
     b = 0
     a_ele = 0
     b_ele = 0
+    dx = 0
+    dy = 0
+
     ! Read in mesh data
     open(newunit=io, file="hw4.ele", status="old", action="read")
-    read(io, *) ele
+    do i = 1, n_ele
+        read(io, *) ele(i, :)
+    end do
     close(io)
 
     open(newunit=io, file="hw4.nod", status="old", action="read")
-    read(io, *) node
+    do i = 1, n_node
+        read(io, *) node(i, :)
+    end do
     close(io)
-   
+
    
     ! element assembly
     do l = 1, n_ele
-        call element_matrix(l, ele, node, a_ele, b_ele)
-        do i = 1, 3
-            ii = ele(l, i+1)
-            b(ii) = b(ii) + b_ele(i)
-
-            do j = 1, 3
-                jj = ele(l, j+1)
-
-                jb = (hbw+ 1) + jj - ii
-
-                a(ii, jb) = a(ii, jb) + a_ele(i, j)
-            end do
-        end do
-    end do
-
-    !! apply boundary conditions
-    !do i = 1, n_node
-    !    if (i == 1 .or. i == 6 .or. i == 11 .or. i == 16) then
-    !        do j = 1, hbw*2 +1
-    !            a(i, j) = 0
-    !        end do
-    !        a(i, hbw + 1) = 1
-    !        b(i) = 0
-
-    !    else if (i == 5 .or. i == 10 .or. i == 15 .or. i == 20 .or. i == 24 .or. i == 27.or. i == 28) then
-    !        do j = 1, hbw*2 +1  
-    !            a(i, j) = 0
-    !        end do
-    !        a(i, hbw + 1) = 1
-    !        b(i) = 1
-    !    
-    !    else if (i < 5) then
-    !        do j = 1, hbw*2 +1  
-    !            a(i, j) = 0
-    !        end do
-    !        a(i, hbw + 1) = 1
-    !        b(i) = (i-1)/4
-    !    else if (i == 21 .or. i == 25) then
-    !        b(i) = 0
-    !    end if 
-    !end do
-    !! solve for the unknowns
-    !call solve(1, a, b, n_node, hbw, n_node, 2*hbw+1)
-
-    !! output solution
-    !open(unit=io, file="output", status="unknown")
-    !do i = 1, n_node
-    !    write(io, *) b(i)
-    !end do 
-
-    end program hw4
-
-    subroutine element_matrix(l, ele, node, a_ele, b_ele)
-        implicit none
-        integer, intent(in) :: l 
-        integer, allocatable, intent(in) :: ele(:, :)
-        real, allocatable, intent(in) :: node(:, :)
-        real, allocatable, intent(out) :: a_ele(:, :), b_ele(:)
-
-
-        ! local variables
-        real :: x1, x2, x3, y1, y2, y3, area, dx1, dx2, dx3, dy1, dy2, dy3, ke = 0
-        
         x1 = node(ele(l, 2), 2)
         x2 = node(ele(l, 3), 2)
         x3 = node(ele(l, 4), 2)
@@ -130,6 +64,103 @@
         a_ele(3, 2) = - (dy3 * dy2)/(4 * area) - (dx3 * dx2)/(4 * area) + (ke * ke * area)/12
         a_ele(3, 3) = - (dy3 * dy3)/(4 * area) - (dx3 * dx3)/(4 * area) + (ke * ke * area)/6
 
-        b_ele = 0
+        b_ele(1) = 0
+        b_ele(2) = 0
+        b_ele(3) = 0
+
+        do i = 1, 3
+            ii = ele(l, i+1)
+            b(ii) = b(ii) + b_ele(i)
+
+            do j = 1, 3
+                jj = ele(l, j+1)
+
+                jb = (hbw+ 1) + jj - ii
+                
+                a(ii, jb) = a(ii, jb) + a_ele(i, j)
+            end do
+        end do
+    end do
+
+    ! apply boundary conditions
+    do i = 1, n_node
+        if (i == 1 .or. i == 6 .or. i == 11 .or. i == 16) then
+            do j = 1, hbw*2 +1
+                a(i, j) = 0
+            end do
+            a(i, hbw + 1) = 1
+            b(i) = 0
+
+        else if (i == 5 .or. i == 10 .or. i == 15 .or. i == 20 .or. i == 24 .or. i == 27.or. i == 28 .or. i == 25 .or. i == 21) then
+            do j = 1, hbw*2 +1  
+                a(i, j) = 0
+            end do
+            a(i, hbw + 1) = 1
+            b(i) = 1
         
-    end subroutine element_matrix
+        else if (i < 5) then
+            do j = 1, hbw*2 +1  
+                a(i, j) = 0
+            end do
+            a(i, hbw + 1) = 1
+            b(i) = (i-1)/4.0
+        !else if ( ) then
+        !    b(i) = 0
+        end if 
+    end do
+
+    ! solve for the unknowns
+    call solve(1, a, b, n_node, hbw, n_node, 2*hbw+1)
+    call solve(2, a, b, n_node, hbw, n_node, 2*hbw+1)
+
+    ! output solution
+    open(unit=7, file="output_2", status="unknown")
+    do i = 1, n_node
+        write(7, *) b(i)
+    end do 
+    close(7)
+
+
+    do l = 1, n_ele
+        x1 = node(ele(l, 2), 2)
+        x2 = node(ele(l, 3), 2)
+        x3 = node(ele(l, 4), 2)
+
+        y1 = node(ele(l, 2), 3)
+        y2 = node(ele(l, 3), 3)
+        y3 = node(ele(l, 4), 3)
+
+        dx1 = x2 - x3
+        dx2 = x3 - x1
+        dx3 = x1 - x2
+        
+        dy1 = y2 - y3
+        dy2 = y3 - y1
+        dy3 = y1 - y2
+
+        area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+        
+        dx(l) = dx(l) + b(ele(l, 2)) * dy1 + b(ele(l, 3)) * dy2 + b(ele(l, 4)) * dy3
+        dy(l) = dy(l) + b(ele(l, 2)) * dx1 + b(ele(l, 3)) * dx2 + b(ele(l, 4)) * dx3
+    end do
+
+
+
+
+
+    ! output derivatives
+    open(unit=8, file="flow_x_2.dat", status="unknown")
+    do i = 1, n_ele
+        write(8, *) dy(i)
+    end do
+    close(8)
+
+    open(unit=9, file="flow_y_2.dat", status="unknown")
+    do i = 1, n_ele
+        write(9, *) -dx(i)
+    end do
+    close(9)
+
+
+    deallocate(ele, node, a, b, a_ele, b_ele, dx, dy)
+    end program hw4
