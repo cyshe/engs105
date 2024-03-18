dbddc5e550d360251ea1d4f18c98cead030bc9fc
matthew.she <matthew.she@mail.utoronto.ca>
2024-03-18 12:00:46 -0400

    hw6

--- hw5/hw5_timestepping.f90
+++ hw5/hw5_timestepping.f90
@@ -1,14 +1,16 @@
     program hw5
     implicit none
 
-    integer :: io=6, n_ele=1089, n_node=969, n_bounds=64, l, i, j, ii, jj, jb, hbw=50, m
+    integer :: io=6, n_ele=1089, n_node=969, n_bounds=64, l, i, j, ii, jj, jb, hbw=50, m,it
     integer, allocatable :: ele(:, :)
-    real, allocatable :: node(:, :), bc(:, :), heating_rate(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:), dx(:), dy(:)
+    real, allocatable :: node(:, :), bc(:, :), heating_rate(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:), dx(:), dy(:), a_rhs(:, :)
+    real, allocatable :: a_ele_r(:, :), b_prev(:), rhs(:)
     real :: x1, x2, x3, x4, y1, y2, y3, y4, area, dx1, dx2, dx3, dy1, dy2, dy3, ke = 0, km, mm, heating_rate_m, l1, l2
-    real :: material(6,3), gauss_points(4, 2), z, e, phi(4), dpx(4), dpy(4), dj, xs(4), ys(4)
+    real :: material(6,3), gauss_points(4, 2), z, e, phi(4), dpx(4), dpy(4), dj, xs(4), ys(4), theta, dt, pc
 
     allocate(ele(n_ele, 6), node(n_node, 3), bc(n_bounds, 7), heating_rate(n_ele, 3))
     allocate(a(n_node, 2*hbw+1), b(n_node), a_ele(4, 4), b_ele(4), dx(n_ele), dy(n_ele))
+    allocate(a_rhs(n_node, 2*hbw+1), a_ele_r(4, 4), b_prev(n_node), rhs(n_node))
 
     ele = 0
     node = 0
@@ -18,8 +20,15 @@
     b = 0
     a_ele = 0
     b_ele = 0
+    a_ele_r = 0
+    a_rhs = 0
+    b_prev = 0
+    rhs = 0
+
     dx = 0
     dy = 0
+    theta = 0.5
+    dt = 0.05
 
     ! kappa, c* rho, m for each material
     material(1, 1) = 0.210
@@ -83,6 +92,8 @@
             ! triangular element
             mm = material(ele(l, 6)-2, 3)
             ke = material(ele(l, 6)-2, 1)
+            pc = material(ele(l, 6)-2, 2)
+
 
             x1 = node(ele(l, 2), 2)
             x2 = node(ele(l, 3), 2)
@@ -102,22 +113,29 @@
 
             area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
 
-            a_ele(1, 1) = - ke * (dy1 * dy1)/(4 * area) - ke * (dx1 * dx1)/(4 * area) - (mm * area)/6
-            a_ele(1, 2) = - ke * (dy1 * dy2)/(4 * area) - ke * (dx1 * dx2)/(4 * area) - (mm * area)/12
-            a_ele(1, 3) = - ke * (dy1 * dy3)/(4 * area) - ke * (dx1 * dx3)/(4 * area) - (mm * area)/12
+            a_ele(1, 1) = (-ke * (dy1 * dy1)/(4 * area) -ke * (dx1 * dx1)/(4 * area) - (mm * area)/6)*dt*theta - (pc * area)/6
+            a_ele(1, 2) = (-ke * (dy1 * dy2)/(4 * area) -ke * (dx1 * dx2)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(1, 3) = (-ke * (dy1 * dy3)/(4 * area) -ke * (dx1 * dx3)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(2, 1) = (-ke * (dy2 * dy1)/(4 * area) -ke * (dx2 * dx1)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(2, 2) = (-ke * (dy2 * dy2)/(4 * area) -ke * (dx2 * dx2)/(4 * area) - (mm * area)/6)*dt*theta - (pc * area)/6
+            a_ele(2, 3) = (-ke * (dy2 * dy3)/(4 * area) -ke * (dx2 * dx3)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(3, 1) = (-ke * (dy3 * dy1)/(4 * area) -ke * (dx3 * dx1)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(3, 2) = (-ke * (dy3 * dy2)/(4 * area) -ke * (dx3 * dx2)/(4 * area) - (mm * area)/12)*dt*theta- (pc * area)/12
+            a_ele(3, 3) = (-ke * (dy3 * dy3)/(4 * area) -ke * (dx3 * dx3)/(4 * area) - (mm * area)/6)*dt*theta- (pc * area)/6
 
+            a_ele_r(1, 1)=(-ke * (dy1 * dy1)/(4*area)-ke * (dx1 * dx1)/(4*area) - (mm*area)/6)*dt*(1-theta) - (pc * area)/6
+            a_ele_r(1, 2)=(-ke * (dy1 * dy2)/(4*area)-ke * (dx1 * dx2)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(1, 3)=(-ke * (dy1 * dy3)/(4*area)-ke * (dx1 * dx3)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(2, 1)=(-ke * (dy2 * dy1)/(4*area)-ke * (dx2 * dx1)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(2, 2)=(-ke * (dy2 * dy2)/(4*area)-ke * (dx2 * dx2)/(4*area) - (mm*area)/6)*dt*(1-theta) - (pc * area)/6
+            a_ele_r(2, 3)=(-ke * (dy2 * dy3)/(4*area)-ke * (dx2 * dx3)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(3, 1)=(-ke * (dy3 * dy1)/(4*area)-ke * (dx3 * dx1)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(3, 2)=(-ke * (dy3 * dy2)/(4*area)-ke * (dx3 * dx2)/(4*area) - (mm*area)/12)*dt*(1-theta)- (pc * area)/12
+            a_ele_r(3, 3)=(-ke * (dy3 * dy3)/(4*area)-ke * (dx3 * dx3)/(4*area) - (mm*area)/6)*dt*(1-theta) - (pc * area)/6
 
-            a_ele(2, 1) = - ke * (dy2 * dy1)/(4 * area) - ke * (dx2 * dx1)/(4 * area) - (mm * area)/12
-            a_ele(2, 2) = - ke * (dy2 * dy2)/(4 * area) - ke * (dx2 * dx2)/(4 * area) - (mm * area)/6
-            a_ele(2, 3) = - ke * (dy2 * dy3)/(4 * area) - ke * (dx2 * dx3)/(4 * area) - (mm * area)/12
-
-            a_ele(3, 1) = - ke * (dy3 * dy1)/(4 * area) - ke * (dx3 * dx1)/(4 * area) - (mm * area)/12
-            a_ele(3, 2) = - ke * (dy3 * dy2)/(4 * area) - ke * (dx3 * dx2)/(4 * area) - (mm * area)/12
-            a_ele(3, 3) = - ke * (dy3 * dy3)/(4 * area) - ke * (dx3 * dx3)/(4 * area) - (mm * area)/6
-
-            b_ele(1) = -heating_rate(l, 3) * area / 3
-            b_ele(2) = -heating_rate(l, 3) * area / 3
-            b_ele(3) = -heating_rate(l, 3) * area / 3
+            b_ele(1) = -heating_rate(l, 3) * area * dt / 3
+            b_ele(2) = -heating_rate(l, 3) * area * dt / 3
+            b_ele(3) = -heating_rate(l, 3) * area * dt / 3
             
             do i = 1, 3
                 ii = ele(l, i+1)
@@ -129,6 +147,7 @@
                     jb = (hbw+ 1) + jj - ii
 
                     a(ii, jb) = a(ii, jb) + a_ele(i, j)
+                    a_rhs(ii, jb) = a_rhs(ii, jb) + a_ele_r(i, j)
                 end do
             end do
         else 
@@ -165,20 +184,25 @@
                 heating_rate_m = 0
                 km = 0
                 mm = 0
+                pc = 0
                 call basis(phi, dpx, dpy, dj, z, e, xs, ys)
 
                 ! assemble coefficients 
                 do i = 1, 4
                     km = km + material(ele(l, 6)-2, 1) * phi(i)
                     mm = mm + material(ele(l, 6)-2, 3) * phi(i)
+                    pc = pc + material(ele(l, 6)-2, 2) * phi(i)
                     heating_rate_m = heating_rate_m + heating_rate(l, 3) * phi(i)
                 end do
 
                 do i = 1, 4
                     do j = 1, 4
-                    a_ele(i, j) = a_ele(i, j) + dj * (-km * (dpx(i) * dpx(j) + dpy(i) * dpy(j)) - (mm * phi(i) * phi(j)))
+                    a_ele(i,j) = a_ele(i,j) + dj*(-km *dt*theta*(dpx(i)*dpx(j)+dpy(i)*dpy(j)) &
+                        -((dt*theta*mm+pc)*phi(i)*phi(j)))
+                    a_ele_r(i,j)=a_ele_r(i,j) +dj*(km*dt*(1-theta)*(dpx(i)*dpx(j)+dpy(i)*dpy(j)) &
+                        +((dt*(1-theta)*mm-pc)*phi(i)*phi(j)))
                     end do
-                    b_ele(i) = b_ele(i) - dj * heating_rate_m * phi(i) 
+                    b_ele(i) = b_ele(i) - dj * heating_rate_m * phi(i) * dt
                 end do
             end do 
             do i = 1, 4
@@ -190,50 +214,78 @@
                     jb = (hbw+ 1) + jj - ii
 
                     a(ii, jb) = a(ii, jb) + a_ele(i, j)
+                    a_rhs(ii, jb) = a_rhs(ii, jb) + a_ele_r(i, j)
                 end do
             end do
         end if 
        
     end do
 
-    ! apply boundary conditions
+     do i = 1, n_bounds
+         ii = int(bc(i, 2))
+         do j = 1, 2 * hbw + 1
+             a(ii, j) = 0
+             a_rhs(ii, j) = 0
+         end do
+         a(ii, hbw+1) = 1
+         a_rhs(ii, hbw+1) = 1
+         b(ii) = 0
+     end do 
+    ! apply boundary conditions 
+
     !do i = 1, n_bounds
     !    ii = int(bc(i, 2))
-    !    do j = 1, 2 * hbw + 1
-    !        a(ii, j) = 0
-    !    end do
-    !    a(ii, hbw+1) = 1
-    !    b(ii) = 0
-    !end do 
-    
-    do i = 1, n_bounds
-        ii = int(bc(i, 2))
-        l1 = sqrt((node(ii,2)-node(int(bc(i,4)),2))**2+(node(ii,3)-node(int(bc(i,4)),3))**2)
-        l2 = sqrt((node(ii,2)-node(int(bc(i,5)),2))**2+(node(ii,3)-node(int(bc(i,5)),3))**2)
-        b(ii) = b(ii)-0.5*l1*bc(i,6)*bc(i,7)/material(1,1)
-        b(ii) = b(ii)-0.5*l2*bc(i,6)*bc(i,7)/material(1,1)
-        A(ii, hbw+1) = A(ii, hbw+1) - (l1+l2)*bc(i,6)/material(1,1)/3
+    !    l1 = sqrt((node(ii,2)-node(int(bc(i,4)),2))**2+(node(ii,3)-node(int(bc(i,4)),3))**2)
+    !    l2 = sqrt((node(ii,2)-node(int(bc(i,5)),2))**2+(node(ii,3)-node(int(bc(i,5)),3))**2)
+    !    b(ii) = b(ii)-0.5*l1*bc(i,6)*bc(i,7)/material(1,1) * dt
+    !    b(ii) = b(ii)-0.5*l2*bc(i,6)*bc(i,7)/material(1,1) * dt
+    !    a(ii, hbw+1) = a(ii, hbw+1) - (l1+l2)*bc(i,6)/material(1,1)/3 *dt * theta
 
-        jb = (hbw+1) + int(bc(i,4)) - ii
-        A(ii, jb) = A(ii, jb) - l1*bc(i,6)/material(1,1)/6
+    !    jb = (hbw+1) + int(bc(i,4)) - ii
+    !    a(ii, jb) = a(ii, jb) - l1*bc(i,6)/material(1,1)/6 *dt * theta
 
-        jb = (hbw+1) + int(bc(i,5)) - ii
-        A(ii, jb) = A(ii, jb) - l2*bc(i,6)/material(1,1)/6
-
-    end do
+    !    jb = (hbw+1) + int(bc(i,5)) - ii
+    !    a(ii, jb) = a(ii, jb) - l2*bc(i,6)/material(1,1)/6 * dt * theta
+    !end do
 
     ! solve for the unknowns
     call solve(1, a, b, n_node, hbw, n_node, 2*hbw+1)
-    call solve(2, a, b, n_node, hbw, n_node, 2*hbw+1)
     
-    ! output solution
-    open(unit=7, file="output_3", status="unknown")
+
     do i = 1, n_node
-        write(7, *) b(i)
+        rhs(i) = b(i)
+    end do
+
+    b = 0.0
+
+    do i = 1, 3
+        do j = 1, n_node
+            b_prev(j) = b(j)
+        end do
+
+        b = 0.0
+        do j = 1, n_node
+            do it = 1, 2*hbw+1
+                if (j + it - hbw - 1 > 0 .and. j + it - hbw - 1 <= n_node) then
+                    b(j) = b(j) + a_rhs(j, it) * b_prev(j + it - hbw - 1) 
+                end if
+            end do
+        end do
+
+        do j = 1, n_node
+            b(j) = b(j) + rhs(j)
+        end do
+
+
+        call solve(2, a, b, n_node, hbw, n_node, 2*hbw+1)
+    end do        
+    open(unit=7, file="output_3", status="unknown")
+    do j = 1, n_node
+        write(7, *) b(j)
     end do 
     close(7)
 
-    deallocate(ele, node, a, b, a_ele, b_ele, dx, dy)
+    deallocate(ele, node, a, b, a_ele, b_ele, dx, dy, a_rhs, a_ele_r, b_prev, rhs)
     end program hw5
 
 
--- hw6/hw6.f90
+++ hw6/hw6.f90
@@ -1,12 +1,17 @@
     program hw6
     implicit none
 
-    integer :: io=6, n_ele = 38, n_node=28, l, i, j, ii, jj, jb, hbw=6
+    integer :: io=6, n_ele = 917, n_node=502, l, i, j, ii, jj, jb, hbw=30, n_bc=109, n_gd=33, ipiv, info
     integer, allocatable :: ele(:, :)
-    real, allocatable :: node(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:), dx(:), dy(:)
-    real :: x1, x2, x3, y1, y2, y3, area, dx1, dx2, dx3, dy1, dy2, dy3, ke = 0
+    real, allocatable :: node(:, :), a(:, :), b(:), a_ele(:, :), b_ele(:), dx(:), dy(:), bc(:, :), gd(:, :)
+    real, allocatable :: a_full(:, :), b_full(:)
 
-    allocate(ele(n_ele, 5), node(n_node, 3), a(n_node, n_node), b(n_node), a_ele(3, 3), b_ele(3), dx(n_ele), dy(n_ele))
+    real :: x1, x2, x3, y1, y2, y3, area, dx1, dx2, dx3, dy1, dy2, dy3, x, y
+
+    allocate(ele(n_ele, 5), node(n_node, 3), a(n_node, n_node), b(n_node), a_ele(3, 3), b_ele(3), dx(n_ele), dy(n_ele), &
+        gd(n_gd,2), bc(n_bc,5))
+    
+    allocate(a_full(n_node, n_node), b_full(n_node))
     
     ele = 0
     node = 0
@@ -30,6 +35,18 @@
     end do
     close(io)
 
+    open(newunit=io, file="hw44.bel", status="old", action="read")
+    do i = 1, n_bc
+        read(io, *) bc(i, :)
+    end do
+    close(io)
+
+    open(newunit=io, file="hw44.dnd", status="old", action="read")
+    do i = 1, n_gd
+        read(io, *) gd(i, :)
+    end do
+    close(io)
+
    
     ! element assembly
     do l = 1, n_ele
@@ -49,24 +66,36 @@
         dy2 = y3 - y1
         dy3 = y1 - y2
 
+        x = (x1 + x2 + x3)/3
+        y = (y1 + y2 + y3)/3
+
         area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
 
-        a_ele(1, 1) = - (dy1 * dy1)/(4 * area) - (dx1 * dx1)/(4 * area) + (ke * ke * area)/6
-        a_ele(1, 2) = - (dy1 * dy2)/(4 * area) - (dx1 * dx2)/(4 * area) + (ke * ke * area)/12
-        a_ele(1, 3) = - (dy1 * dy3)/(4 * area) - (dx1 * dx3)/(4 * area) + (ke * ke * area)/12
-
+        a_ele(1, 1) = (1 + x/10 + y/5) * (-(dy1 * dy1)/(4 * area) - (dx1 * dx1)/(4 * area))
+        a_ele(1, 2) = (1 + x/10 + y/5) * (-(dy1 * dy2)/(4 * area) - (dx1 * dx2)/(4 * area))
+        a_ele(1, 3) = (1 + x/10 + y/5) * (-(dy1 * dy3)/(4 * area) - (dx1 * dx3)/(4 * area))
         
-        a_ele(2, 1) = - (dy2 * dy1)/(4 * area) - (dx2 * dx1)/(4 * area) + (ke * ke * area)/12
-        a_ele(2, 2) = - (dy2 * dy2)/(4 * area) - (dx2 * dx2)/(4 * area) + (ke * ke * area)/6
-        a_ele(2, 3) = - (dy2 * dy3)/(4 * area) - (dx2 * dx3)/(4 * area) + (ke * ke * area)/12
+        a_ele(2, 1) = (1 + x/10 + y/5) * (-(dy2 * dy1)/(4 * area) - (dx2 * dx1)/(4 * area))
+        a_ele(2, 2) = (1 + x/10 + y/5) * (-(dy2 * dy2)/(4 * area) - (dx2 * dx2)/(4 * area))
+        a_ele(2, 3) = (1 + x/10 + y/5) * (-(dy2 * dy3)/(4 * area) - (dx2 * dx3)/(4 * area))
         
-        a_ele(3, 1) = - (dy3 * dy1)/(4 * area) - (dx3 * dx1)/(4 * area) + (ke * ke * area)/12
-        a_ele(3, 2) = - (dy3 * dy2)/(4 * area) - (dx3 * dx2)/(4 * area) + (ke * ke * area)/12
-        a_ele(3, 3) = - (dy3 * dy3)/(4 * area) - (dx3 * dx3)/(4 * area) + (ke * ke * area)/6
+        a_ele(3, 1) = (1 + x/10 + y/5) * (-(dy3 * dy1)/(4 * area) - (dx3 * dx1)/(4 * area))
+        a_ele(3, 2) = (1 + x/10 + y/5) * (-(dy3 * dy2)/(4 * area) - (dx3 * dx2)/(4 * area))
+        a_ele(3, 3) = (1 + x/10 + y/5) * (-(dy3 * dy3)/(4 * area) - (dx3 * dx3)/(4 * area))
 
         b_ele(1) = 0
         b_ele(2) = 0
         b_ele(3) = 0
+        
+        
+        ! source element ele #288
+        ! 151 167 150
+        if (l == 288) then
+            ! -0.206052,  0.56379
+            b_ele(1) = -(x2*y3-x3*y2+dy1*(-0.206052)-dx1*0.56379)/(2 * area)
+            b_ele(2) = -(x3*y1-x1*y3+dy2*(-0.206052)-dx2*0.56379)/(2 * area)
+            b_ele(3) = -(x1*y2-x2*y1+dy3*(-0.206052)-dx3*0.56379)/(2 * area)
+        end if
 
         do i = 1, 3
             ii = ele(l, i+1)
@@ -82,44 +111,46 @@
         end do
     end do
 
+    
+
     ! apply boundary conditions
-    do i = 1, n_node
-        if (i == 1 .or. i == 6 .or. i == 11 .or. i == 16) then
-            do j = 1, hbw*2 +1
-                a(i, j) = 0
-            end do
-            a(i, hbw + 1) = 1
-            b(i) = 0
-
-        else if (i == 5 .or. i == 10 .or. i == 15 .or. i == 20 .or. i == 24 .or. i == 27.or. i == 28 .or. i == 25) then
-            do j = 1, hbw*2 +1  
-                a(i, j) = 0
-            end do
-            a(i, hbw + 1) = 1
-            b(i) = 1
-        
-        else if (i < 5) then
-            do j = 1, hbw*2 +1  
-                a(i, j) = 0
-            end do
-            a(i, hbw + 1) = 1
-            b(i) = (i-1)/4.0
-        !else if ( ) then
-        !    b(i) = 0
-        end if 
+    do i = 1, n_gd
+        ii = INT(gd(i, 1))
+        b(ii) = 0.0
+        do j = 1, 2*hbw+1
+            a(ii, j) = 0
+        end do
+        a(ii, hbw+1) = 1
     end do
-
+    
+    
+    ! source element ele #278 (on boundary)
+    b(492) = b(492) + (1 + node(492,2)/10 + node(492,3)/5) * 0.25/3
+    b(493) = b(493) + (1 + node(492,2)/10 + node(492,3)/5) * 0.25/3
+    
+    ! copy band matrix to full matrix
+    do i = 1, n_node
+        do j = 1, 2*hbw+1
+            if (i+j-hbw-1 > 0 .and. i+j-hbw-1 <= n_node) then
+                a_full(i, j+i-hbw-1) = a(i, j)
+            end if
+        end do
+    end do
+    
+    
+    
     ! solve for the unknowns
     call solve(1, a, b, n_node, hbw, n_node, 2*hbw+1)
     call solve(2, a, b, n_node, hbw, n_node, 2*hbw+1)
-
     ! output solution
     open(unit=7, file="output_1", status="unknown")
     do i = 1, n_node
         write(7, *) b(i)
     end do 
     close(7)
-
+    
+    !call SGETRF(n_node, n_node, a_full, n_node, ipiv, info)
+    !call SGETRS
 
     do l = 1, n_ele
         x1 = node(ele(l, 2), 2)
@@ -144,20 +175,20 @@
         dy(l) = -b(ele(l, 2)) * dx1 - b(ele(l, 3)) * dx2 - b(ele(l, 4)) * dx3
     end do
 
-
     ! output derivatives
     open(unit=8, file="flow_x_1.dat", status="unknown")
     do i = 1, n_ele
-        write(8, *) dy(i)
+        write(8, *) dx(i)
     end do
     close(8)
 
     open(unit=9, file="flow_y_1.dat", status="unknown")
     do i = 1, n_ele
-        write(9, *) -dx(i)
+        write(9, *) dy(i)
     end do
     close(9)
-
-
+    
+    
+    
     deallocate(ele, node, a, b, a_ele, b_ele, dx, dy)
     end program hw6
